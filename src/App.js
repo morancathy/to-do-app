f48d5ef2c944ecfcadbe4db6970a8280e968db71
Cat <morancathy@gmail.com>
2021-07-26 20:58:31 -0500

    add button toggles view of form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks,setTasks] = useState([
     {
       id: 1,
@@ -53,8 +54,10 @@ const toggleReminder = (id) => {
 }
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask
+        (!showAddTask)}
+        showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0
         ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
         : 'No To Do Items'
