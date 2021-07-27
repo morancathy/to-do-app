bd7e126d96bdea50d823b26139964cfd24503daf
Cat <morancathy@gmail.com>
2021-07-26 22:24:21 -0500

    router work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './styles.css';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState (false)
@@ -84,16 +87,24 @@ const toggleReminder = async (id) => {
 }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask
-        (!showAddTask)}
-        showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0
-        ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-        : 'No To Do Items'
-      }
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask
+          (!showAddTask)}
+          showAdd={showAddTask}/>
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0
+              ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+              : 'No To Do Items'
+            }
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
