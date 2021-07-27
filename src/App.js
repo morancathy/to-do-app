455304566db329ad24e13690f00a3f5c21374a8d
Cat <morancathy@gmail.com>
2021-07-26 20:11:27 -0500

    delete item works

--- src/App.js
+++ src/App.js
@@ -32,10 +32,25 @@ function App() {
     }
   ]
 )
+//delete task         onDelete is a prop of Tasks, so need to pass it in
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) =>
+    task.id === id
+    ? {...task, reminder: !task.reminder}
+    : task))
+}
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0
+        ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        : 'No To Do Items'
+      }
       <NewForm />
       <ToDo />
     </div>
