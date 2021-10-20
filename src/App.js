b8351dc4e62e359b0ffe58f7780eeeb443831bd5
Cat <morancathy@gmail.com>
2021-10-20 13:23:27 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -37,43 +37,43 @@ function App() {
 /*################     Everything commented is the extra work on the Brad Traversy tutorial, working with routing. Everything worked with the routing.
 But when I shut everything down and revisited this, it would not link to localhost:5000 anymore. I HAVE NO IDEA WHY  ################################*/
 
-  // useEffect(() => {
-  //   const getTasks = async () => {
-  //     const tasksFromServer = await fetchTasks()
-  //     setTasks(tasksFromServer)
-  //   }
-  //   getTasks()
-  // },[])
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  },[])
 
 // Fetch Tasks
-  // const fetchTasks = async () => {
-  //   const res = await fetch('http://localhost:5000/tasks')
-  //   const data = await res.json()
-  //
-  //   return data
-  // }
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 // Fetch Task
-  // const fetchTask = async (id) => {
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`)
-  //   const data = await res.json()
-  //
-  //   return data
-  // }
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
 
 // Add task
 const addTask = async (task) => {
-  // const res = await fetch('http://localhost:5000/tasks', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-type': 'application/json'
-  //   },
-  //   body: JSON.stringify(task)
-  // })
-  //
-  // const data = await res.json();
-  //
-  // setTasks([...tasks, data])
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json();
+
+  setTasks([...tasks, data])
   const id = Math.floor(Math.random() * 1000) + 1
   const newTask = {id, ...task}
   setTasks([...tasks, newTask])
@@ -81,26 +81,26 @@ const addTask = async (task) => {
 
 // delete task         onDelete is a prop of Tasks, so need to pass it in
 const deleteTask = async (id) => {
-  // await fetch(`http://localhost:5000/tasks/${id}`, {
-  //   method: 'DELETE'
-  // })
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
 // toggle reminder
 const toggleReminder = async (id) => {
-  // const taskToToggle = await fetchTask(id)
-  // const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
 
-  // const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-  //   method: 'PUT',
-  //   headers: {
-  //     'Content-type': 'application/json'
-  //   },
-  //   body: JSON.stringify(updatedTask)
-  // })
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updatedTask)
+  })
 
-  // const data = await res.json()
+  const data = await res.json()
 
   setTasks(
     tasks.map((task) =>
@@ -113,9 +113,8 @@ const toggleReminder = async (id) => {
   return (
     <Router>
       <div className="container">
-        <Header onAdd={() => setShowAddTask
-          (!showAddTask)}
-          showAdd={showAddTask}/>
+        <Header onAdd={() => setShowAddTask (!showAddTask)}
+                showAdd={showAddTask}/>
         <Route path='/' exact render={(props) => (
           <>
             {showAddTask && <AddTask onAdd={addTask}/>}
